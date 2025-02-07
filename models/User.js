f3df3dea729a01c3b5664be4e42e26433e02b951
kseipoll <koblandikz@gmail.com>
2025-02-07 11:54:15 +0500

    Save in bd places

--- models/User.js
+++ models/User.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    selectedPlaces: { type: [String], default: [] } 
+    selectedPlaces: { type: [Object], default: [] },
+    centerCoords: { type: Object, default: null }  // Новое поле для серединной координаты
 });
 
 module.exports = mongoose.model("User", UserSchema);
